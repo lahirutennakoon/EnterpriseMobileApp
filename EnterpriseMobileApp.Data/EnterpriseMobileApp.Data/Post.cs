6f5364fe7a213c81eeb047491809e55e564c60e1
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2018-09-20 00:03:57 +0530

    installed newtonsoft package

--- EnterpriseMobileApp.Data/EnterpriseMobileApp.Data/Post.cs
+++ EnterpriseMobileApp.Data/EnterpriseMobileApp.Data/Post.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace EnterpriseMobileApp.Data
 {
     class Post
     {
+        public string UserId { get; set; }
     }
 }
